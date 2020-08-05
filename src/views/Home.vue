aeccf361d716493631abcbdd18ead82e345527d3
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-05 19:16:03 +0000

    hero section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-
+  <HeroSection/>
   </div>
 </template>
 
 <script>
+import HeroSection from '@/components/hero.vue'
   export default {
-    
+    components:{
+      HeroSection
+    }
   }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
